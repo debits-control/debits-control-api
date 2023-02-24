506a2a98cfef402f12e9ffb22100acec32a03812
Nailton Oliveira <nailton.me91@gmail.com>
2023-02-23 22:35:08 -0300

    refactor: remove unused package

--- app/factory/payment_type_factory.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-from app.repository.payment_type_repository import PaymentTypeRepository
-from app.services.payment_type_service import PaymentTypeService
-
-
-def payment_type_factory(session: Session):
-    repository = PaymentTypeRepository(session)
-    return PaymentTypeService(repository)
--- app/factory/user_factory.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-from app.repository.user_repository import UserRepository
-from app.services.user_service import UserService
-
-
-def user_factory(session: Session):
-    repository = UserRepository(session)
-    return UserService(repository)
