fa130abd3cdd66de97d6848237f81483b91fff02
Nailton Oliveira <nailton.me91@gmail.com>
2023-02-26 00:04:08 -0300

    feat: add new routers to add PaymentType and Debit

--- app/routers/debit_routers.py
+++ app/routers/debit_routers.py
@@ -2,12 +2,16 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
 from app.core.container import Container
-from app.schema.debit_schema import Debit
+from app.core.dependency import get_current_user
+from app.core.security import oauth2_scheme
+from app.schema.debit_schema import Debit, DebitFormData
+from app.schema.user_schema import User
 from app.services.debit_service import DebitService
 
 debit_router = APIRouter(
     prefix='/debit',
-    tags=['debit']
+    tags=['debit'],
+    dependencies=[Depends(oauth2_scheme)]
 )
 
 
@@ -26,3 +30,13 @@ async def get_debit_by_id(
         service: DebitService = Depends(Provide[Container.debit_service])
 ):
     return service.get_by_id(debit_id)
+
+
+@debit_router.post('', response_model=Debit)
+@inject
+async def create_debit(
+        debit_form_data: DebitFormData,
+        service: DebitService = Depends(Provide[Container.debit_service]),
+        current_user: User = Depends(get_current_user),
+):
+    return service.add_with_owner(debit_form_data, current_user)
--- app/routers/payment_type_routers.py
+++ app/routers/payment_type_routers.py
@@ -2,12 +2,16 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
 from app.core.container import Container
-from app.schema.payment_type_schema import PaymentType
+from app.core.dependency import get_current_user
+from app.core.security import oauth2_scheme
+from app.schema.payment_type_schema import PaymentType, PaymentTypeFormData
+from app.schema.user_schema import User
 from app.services.payment_type_service import PaymentTypeService
 
 payment_type_router = APIRouter(
     prefix='/payment-type',
-    tags=['payment-type']
+    tags=['payment-type'],
+    dependencies=[Depends(oauth2_scheme)],
 )
 
 
@@ -26,3 +30,13 @@ async def get_payment_type_by_id(
         service: PaymentTypeService = Depends(Provide[Container.payment_type_service])
 ):
     return service.get_by_id(payment_type_id)
+
+
+@payment_type_router.post('', response_model=PaymentType)
+@inject
+async def create_payment_type(
+        payment_type_form_data: PaymentTypeFormData,
+        service: PaymentTypeService = Depends(Provide[Container.payment_type_service]),
+        current_user: User = Depends(get_current_user),
+):
+    return service.add_with_owner(payment_type_form_data, current_user)
